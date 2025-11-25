9d163fe9833395394627c4888fe76397a58ab767
Ayush kushwaha <ayush.kushwaha.29.10.2004@gmail.com>
2025-11-25 20:35:36 +0530

    Reorganize imports and update Flask app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from app import routes  # ye niche hona chahiye, app ke baad
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///career.db'
-    
+    app = Flask(__name__, static_folder='static', template_folder='templates')
+
     db.init_app(app)
     
     from .routes import main
@@ -15,5 +16,5 @@ def create_app():
     
     with app.app_context():
         db.create_all()
-        
+
     return app
