337956e315565b74a976a901be1f8884eb41ffc6
Ayush Kushwaha <ayush.kushwaha.29.10.2004@gmail.com>
2025-11-30 20:21:42 +0530

    Fix SQLAlchemy model + clean structure

--- run.py
+++ run.py
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
-from app import app
-from datetime import datetime
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run()
-=======
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-    from datetime import datetime
->>>>>>> ae39b29 (Update files)
-
-@app.context_processor
-def inject_year():
-    return {'current_year': datetime.now().year}
